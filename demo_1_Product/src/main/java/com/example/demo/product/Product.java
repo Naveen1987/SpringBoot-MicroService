3ef9883af2902bc93f56c7ec55b84e03c71962e8
Naveen New <User-2@gmail.com>
2017-08-10 12:07:16 +0530

    Suceess full Example

---
+++ demo_1_Product/src/main/java/com/example/demo/product/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.product;
+public class Product {
+private long id;
+private String sku;
+private String description;
+public long getId() {
+return id;
+}
+public void setId(long id) {
+this.id = id;
+}
+public String getSku() {
+return sku;
+}
+public void setSku(String sku) {
+this.sku = sku;
+}
+public String getDescription() {
+return description;
+}
+public void setDescription(String description) {
+this.description = description;
+}
+}
